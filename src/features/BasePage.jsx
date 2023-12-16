8ae1a83ed209e343052f377fca1a251ee8d22ea8
ChrisRothove <vacantorange@gmail.com>
2023-12-16 13:31:59 -0600

    added better consistency with back buttons

--- src/features/BasePage.jsx
+++ src/features/BasePage.jsx
@@ -11,7 +11,16 @@ export function BasePage({
     <>
       <h1 className="page-title">{title}</h1>
       <div className="content-horizontal">
-        {backButton && <button onClick={() => navigate(-1)}>Back</button>}
+        {backButton && (
+          <div className="random-item-button-block">
+            <button className="left-button" onClick={() => navigate(-1)}>
+              Back
+            </button>
+            <button className="right-button" onClick={() => navigate("/")}>
+              Home
+            </button>
+          </div>
+        )}
         {buttonList.map((btn) => {
           return (
             <button key={btn.label} onClick={btn.onClickFn}>
