59e5132fd62a37659da03e13dc582e6c7ce43258
ChrisRothove <vacantorange@gmail.com>
2024-04-24 20:51:54 -0500

    add headers to service config

--- src/http/serviceConfig.js
+++ src/http/serviceConfig.js
@@ -2,4 +2,9 @@ import axios from "axios";
 
 export const serviceConfig = axios.create({
   baseURL: process.env.SERVER_URL,
+  timeout: 1000,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,POST,DELETE",
+  },
 });
