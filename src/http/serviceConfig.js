22aab597c80291299098c242c7e75fd74c42a10d
ChrisRothove <vacantorange@gmail.com>
2024-04-24 21:29:08 -0500

    removed service timeout

--- src/http/serviceConfig.js
+++ src/http/serviceConfig.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const serviceConfig = axios.create({
   baseURL: process.env.SERVER_URL,
-  timeout: 1000,
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,POST,DELETE",
